2098401347697dff643f37d8958f0a60f4d96a3a
nonginjun <73819188+nonginjun@users.noreply.github.com>
2021-11-26 14:39:18 +0700

    Update covid.php

--- covid.php
+++ covid.php
@@ -3,12 +3,8 @@
   $data = json_decode($json);
 
   echo "<table border='1'>";
-    <?php
         for($i=0;$i<2;$i++){
-          echo "<tr>";
-          echo "<td>txn_date 2021-11-26</td>";
-          echo "</tr>";
-
+          }
   foreach ( $data[0] as $key => $val ){
     echo $key.":".$val."<br>";
   }
