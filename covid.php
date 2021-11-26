7c5c1783a6e38d5187e2fa0381cdad87eb0cf29b
nonginjun <73819188+nonginjun@users.noreply.github.com>
2021-11-26 14:45:35 +0700

    Update covid.php

--- covid.php
+++ covid.php
@@ -5,18 +5,6 @@
   echo "<table border='2'>";
     <?php
         for($i=0;$i<10;$i++){
-          echo "<tr>";
-          echo "<td>txn_date:2021-11-26</td>";
-          echo "<td>new_case:6559</td>";
-          echo "<td>total_case:2094886</td>";
-          echo "<td>new_case_excludeabroad:6520</td>";
-          echo "<td>total_case_excludeabroad:2088106</td>";
-          echo "<td>new_death:64</td>";
-          echo "<td>total_death:20645</td>";
-          echo "<td>new_recovered:6875</td>";
-          echo "<td>total_recovered:1994183</td>";
-          echo "<td>update_date:2021-11-26 07:32:54</td>";
-          echo "</tr>";
           }
         ?>
     </table>
