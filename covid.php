7e5925fdf4b1dacd3316ea4b8df752b31c50f2a0
nonginjun <73819188+nonginjun@users.noreply.github.com>
2021-11-26 15:55:29 +0700

    Update covid.php

--- covid.php
+++ covid.php
@@ -8,16 +8,9 @@
 
   foreach ( $data[0] as $key => $val ){
         echo "<tr>";
-        echo "<td>";
-        echo $json;
-        echo "</td>";
+        echo "<td>;$key</td>"
         echo "</tr>";
-        echo "<tr>";
-        echo "<td>";
-        echo $data;
-        echo "</td>";
-        echo "</tr>";
-        
+       
   }
 
   echo "</table>";
