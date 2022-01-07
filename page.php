c605ec9bacff741f835cdab85a5296d19b813a7d
nonginjun <73819188+nonginjun@users.noreply.github.com>
2022-01-07 14:21:34 +0700

    Update page.php

--- page.php
+++ page.php
@@ -4,10 +4,9 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <title>Hello, world!</title>]
+    <title>Hello, world!</title>
     
     <style>
       #box1{
@@ -29,13 +28,13 @@
     
     <div class="container">
       <div class="row">
-        <div class="col" id="box1">
+        <div class="col-2" id="box1">
           Column
         </div>
-        <div class="col" id="box2">
+        <div class="col-8" id="box2">
           Column
         </div>
-        <div class="col" id="box3">
+        <div class="col-2" id="box3">
           Column
         </div>
       </div>
