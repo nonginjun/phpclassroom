261c080bb6c7d7b4ad83f588107eade5e4886256
nonginjun <73819188+nonginjun@users.noreply.github.com>
2022-01-21 14:03:51 +0700

    Update template1.php

--- template1.php
+++ template1.php
@@ -24,7 +24,11 @@
           <h1>Header</h1>
         </div>
       </div>
-    </div>  
+     <div class="row">
+        <div class="col-3" id="sidebar"><!-- ไส้บา -->
+          <h1>Header</h1>
+        </div>
+      </div>
     <!-- Option 1: Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
