645ad2efe3c4f302ef12396c9c42f78fd1068565
nonginjun <73819188+nonginjun@users.noreply.github.com>
2022-01-21 14:11:30 +0700

    Update template1.php

--- template1.php
+++ template1.php
@@ -20,10 +20,10 @@
       }
       #content{
         height: 400px;
-        background-color: #FFBBDA;
+        background-color: #C0E4F6;
       }
       header{
-        height: 120px;
+        height: 170px;
         background-color: pink;
     </style>
     
@@ -42,9 +42,12 @@
        <div class="col-9" id="content"><!-- คอนเท้น -->
           <h1>content</h1>
       </div>
+        </div>
+       <div class="row">
        <div class="col-12" id="footer"><!-- ฟุตเต้อ -->
           <h1>footer</h1>
         </div>
+          </div>
     <!-- Option 1: Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
