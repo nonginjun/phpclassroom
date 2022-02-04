e9fd52d85f5b7dc42fd100c63bd45f5cef664d8a
nonginjun <73819188+nonginjun@users.noreply.github.com>
2022-02-04 15:26:46 +0700

    Update font.php

--- font.php
+++ font.php
@@ -6,12 +6,17 @@
         font-family : tkfont;
         src : url(jaedo.woff);
       }
-      body{
+      #font1{
         font-family : tkfont;
       }  
      </style> 
   </head>
   <body>
+    <div id="font1">
     <h1>เว็บไซต์ของฉัน</h1>
+    </div> 
+    <div id="font2">
+    <h1>เว็บไซต์ของฉัน</h1>
+    </div> 
   </body>
 </html>
