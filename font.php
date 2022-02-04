54f073943e1c00fc0338f302a2d058fbc1c15686
nonginjun <73819188+nonginjun@users.noreply.github.com>
2022-02-04 15:32:20 +0700

    Update font.php

--- font.php
+++ font.php
@@ -6,17 +6,23 @@
         font-family : tkfont;
         src : url(jaedo.woff);
       }
+       @font-face{
+        font-family : tk2font;
+        src : url(noren.woff);
       #font1{
         font-family : tkfont;
       }  
+       #font2{
+        font-family : tk2font;
+      }   
      </style> 
   </head>
   <body>
     <div id="font1">
-    <h1>ฟอนต์แบบ1</h1>
+    <h1>นางสาวภัทรวดี ดาบัว เลขที่ 24 ห้อง 5/13</h1>
     </div> 
     <div id="font2">
-    <h2>ฟอนต์แบบ2</h2>
+    <h2>นางสาวภัทรวดี ดาบัว เลขที่ 24 ห้อง 5/13</h2>
     </div> 
   </body>
 </html>
