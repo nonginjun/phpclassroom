73b1db688375160a458474d20cef05f935055152
nonginjun <73819188+nonginjun@users.noreply.github.com>
2022-02-04 15:24:17 +0700

    Update font.php

--- font.php
+++ font.php
@@ -1,6 +1,15 @@
 <html>
   <head>
     <title>การกำหนดรูปแบบตัวอักษร</title>
+    <style>
+      @font-face{
+        font-family : tkfont;
+        src : url(jaedo.woff);
+      }
+      body{
+        font-family : tkfont;
+      }  
+     </style> 
   </head>
   <body>
     <h1>เว็บไซต์ของฉัน</h1>
